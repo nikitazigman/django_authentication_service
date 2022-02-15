6b2ae628f0d3014f6166775087c0d6e0a06baebd
nikitazigman <zigman.nikita@gmail.com>
2022-02-15 18:40:57 +0100

    initial commit

---
+++ service/app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+urlpatterns = [
+    path("", include("dj_rest_auth.urls")),
+    path("registration/", include("dj_rest_auth.registration.urls")),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+]
